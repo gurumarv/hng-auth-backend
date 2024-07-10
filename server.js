f8cb0ffee39c2bcd790210351d6043f8c2d13cc5
Marvellous Giegbefumwen <igmarvvvi@gmail.com>
2024-07-10 13:16:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,14 @@ const startServer = async () => {
       res.send('This is a protected route');
     });
 
+    // Middleware for handling undefined routes
+    app.use((req, res, next) => {
+      res.status(404).json({
+        status: 'error',
+        message: 'Route not found'
+      });
+    });
+
     // Global Error Handler
     app.use((err, req, res, next) => {
       console.error(err.stack);
